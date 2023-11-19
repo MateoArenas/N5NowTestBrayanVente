ec989e93fb9c52e2a1d74772fb030c8691a893b5
MateoArenas <mateo3197@gmail.com>
2023-11-19 13:56:23 -0500

    AddModelsAndEntityFramework
    I add models od Permissions and TypePermissons. Create realtionships and Add DBContext

---
+++ N5NowTestBrayanVente.Domain/Aggregates/PermissionsAggregate/Models/PermissionTypes.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace N5NowTestBrayanVente.Domain.Aggregates.PermissionsAggregate.Models
+{
+    public class PermissionTypes
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<Permissions> Permissions { get; set; }
+    }
+}
---
+++ N5NowTestBrayanVente.Infrastructure/Contexts/N5NowTestDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using N5NowTestBrayanVente.Domain.Aggregates.PermissionsAggregate.Models;
+
+namespace N5NowTestBrayanVente.Infrastructure.Contexts
+{
+    public class N5NowTestDBContext : DbContext
+    {
+        public N5NowTestDBContext(DbContextOptions<N5NowTestDBContext> options) : base(options) { }
+
+        public DbSet<Permissions> Permissions { get; set; }
+        public DbSet<PermissionTypes> PermissionTypes { get; set; }
+    }
+}

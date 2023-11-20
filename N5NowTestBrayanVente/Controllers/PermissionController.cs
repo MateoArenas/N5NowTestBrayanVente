2e2fb0ade41993a191d7363e598664b824552298
MateoArenas <mateo3197@gmail.com>
2023-11-20 11:22:02 -0500

    ImplementationElasticSeach
    Add usings Serilogs, change of program with the configurations and add service, implementation  in controller

--- N5NowTestBrayanVente/Controllers/PermissionController.cs
+++ N5NowTestBrayanVente/Controllers/PermissionController.cs
@@ -11,49 +11,64 @@ namespace N5NowTestBrayanVente.Controllers
     public class PermissionController : Controller
     {
         private readonly IMediator _mediator;
-        public PermissionController(IMediator mediator)
+        private readonly ILogger<PermissionController> _logger;
+        public PermissionController(IMediator mediator, ILogger<PermissionController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<PermissionsResultDTO>> Get(int id)
         {
+            _logger.LogInformation("Iniciando ejecución de Get Permissions");
+
             var permission = await _mediator.Send(new GetPermissionsQuery(id));
             if (permission == null)
             {
+                _logger.LogWarning("Get Permissions - No se encuentran datos");
                 return NotFound();
             }
 
+            _logger.LogInformation("Get Permissions - ejecutado exitosamente");
             return await _mediator.Send(new GetPermissionsQuery(id));
         }
 
         [HttpPost]
         public async Task<ActionResult<PermissionsResultDTO>> Create(RequestPermissionCommand requestPermissionCommand)
         {
+            _logger.LogInformation("Iniciando ejecución de Request Permissions");
+
             PermissionsResultDTO permissionsResultDTO = await _mediator.Send(requestPermissionCommand);
             if (permissionsResultDTO == null)
             {
+                _logger.LogWarning("Request Permissions - No se encuentran datos");
                 return NotFound();
             }
 
+            _logger.LogInformation("Request Permissions ejecutado exitosamente");
             return permissionsResultDTO;
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> Edit(int id, ModifyPermissionCommand modifyPermissionCommand)
         {
+            _logger.LogInformation("Iniciando ejecución de Modify Permissions");
+
             if (id != modifyPermissionCommand.Id)
             {
+                _logger.LogError("Modify Permissions - datos incorrectos");
                 return BadRequest();
             }
 
             PermissionsResultDTO permissionsResultDTO = await _mediator.Send(modifyPermissionCommand);
             if (permissionsResultDTO == null)
             {
+                _logger.LogWarning("Modify Permissions - No se encuentran datos");
                 return NotFound();
             }
 
+            _logger.LogInformation("Modify Permissions ejecutado exitosamente");
             return Ok();
         }
     }
--- N5NowTestBrayanVente/Program.cs
+++ N5NowTestBrayanVente/Program.cs
@@ -3,10 +3,18 @@ using Microsoft.EntityFrameworkCore;
 using N5NowTestBrayanVente.Infrastructure.Contexts;
 using N5NowTestBrayanVente.Infrastructure.Remotes;
 using N5NowTestBrayanVente.Infrastructure.Repositories;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+ConfigureLogs();
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -37,3 +45,31 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+#region helper
+void ConfigureLogs() 
+{
+    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails()
+        .WriteTo.Debug()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(ConfigureELS(configuration, env))
+        .CreateLogger();
+}
+
+ElasticsearchSinkOptions ConfigureELS(IConfigurationRoot configuration, string env)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ELKConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{env.ToLower().Replace(".","-")}-{DateTime.UtcNow:yyyy-MM}"
+    };
+}
+#endregion
